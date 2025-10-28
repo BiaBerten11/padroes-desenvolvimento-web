6b6cae46b3c34e13654543407a92cbef7cbd96b1
Maria Beatriz Berten de Moraes <160761976+BiaBerten@users.noreply.github.com>
2025-10-28 13:45:35 +0000

    27/10

--- projeto/src/main/java/recursos/FabricanteRecurso.java
+++ projeto/src/main/java/recursos/FabricanteRecurso.java
@@ -1,5 +1,16 @@
 package recursos;
 
+import java.util.List;
+
+import entidades.Fabricante;
+import jakarta.ws.rs.GET;
+import jakarta.ws.rs.Path;
+
+@Path("fabricantes")
 public class FabricanteRecurso {
+    @GET
+    public List<Fabricante> listar(){
+        return Fabricante.listAll();
+    }
 
 }
